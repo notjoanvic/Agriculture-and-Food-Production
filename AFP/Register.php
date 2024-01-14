10391939d36f799ac23d2aa9dd3bfa1980638a36
Jaskuno <jasbaet09@gmail.com>
2024-01-14 16:47:56 +0800

    Changes to registration
    - styling
    - Verify in the database whether the username is already taken.
    - Check if the password is less than 8 characters and provide an error if the condition is met.
    -After an error, the form fields that are least important for duplication in the database still remain filled.
    
    (same changes in the login form of both customerLogin and loginAFP(for employee)).

--- AFP/Register.php
+++ AFP/Register.php
@@ -1,113 +1,93 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agricultural Food Production</title>
-<style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agricultural Food Production</title>
+  <style>
     /*BBBBBBBBBBBBBBBOOOOOOOOOOOOOOOOODDDDDDDDDDDDDDDDDDYYYYYYYYYY*/
     * a {
-        text-decoration: none;
-        display: inline-block;
+      text-decoration: none;
+      display: inline-block;
     }
+
     body {
-        background-image: url("https://images.unsplash.com/photo-1586771107445-d3ca888129ff?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-        background-color: beige;
-        background-repeat: no-repeat;
-        background-size: cover;
-        height:100vh;
-        margin: 0;
+      background-image: url("https://images.unsplash.com/photo-1586771107445-d3ca888129ff?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+      background-color: beige;
+      background-repeat: no-repeat;
+      background-size: cover;
+      height: auto;
+      margin: 0;
     }
+
     #mainBody {
-        position: relative;
-        height: auto;
-        width: 100%;
-        border-radius: 10px;
-        background-color: rgba(0, 0, 0, 0.267);
-        /*Top Right Bottom Left */
-        color:rgb(255, 255, 255);
+      position: relative;
+      height: auto;
+      width: 100%;
+      border-radius: 10px;
+      background-color: rgba(0, 0, 0, 0.267);
+      /*Top Right Bottom Left */
+      color: rgb(255, 255, 255);
     }
 
     /*HHHHEEEEEEEEEEEEAAAAAAAAAAAAAAAADDDDDDDDDDDDEEEEEEEEEEEEERRRRRRRRRRR*/
-
     #nav {
-        position:relative;
-        display: flex;
-        background-color: #184911;  
-       
+      position: relative;
+      display: flex;
+      background-color: #184911;
     }
+
     #menu {
-        position: absolute;
-        top:20%;
-        text-align: center;
-        right: 0;
-        padding: 10px 10px 5px 5px;
+      position: absolute;
+      top: 20%;
+      text-align: center;
+      right: 0;
+      padding: 10px 10px 5px 5px;
+      display: inline-flex;
     }
+
     /* liness in between ng menus */
-    #menu a {   
-        margin-right: 10px;
-        border-right: 1px solid grey;
-        font-family: 'Times New Roman';
-        padding: 10px 10px 5px 5px;
-        font-size: 25px;
-        color:white;
-        border-radius: 15px;
-
+    #menu a {
+      margin-right: 10px;
+      border-right: 1px solid grey;
+      font-family: 'Times New Roman';
+      padding: 10px 10px 5px 5px;
+      font-size: 25px;
+      color: white;
+      border-radius: 15px;
     }
+
     #menu a:hover {
-        text-decoration: underline #fdde87c7;
+      text-decoration: underline #fdde87c7;
     }
+
     /*MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIIIIIIIIIIIIIIIIIIIIIIIIIIIINNNNNNNNNNNNN*/
-
     #main {
-        position: relative;
-        display: flex;
-        justify-content: center;
-        margin: 5px;
-        background-color: rgba(0, 0, 0, 0.404);
-        padding: 1vh;
+      position: relative;
+      display: flex;
+      margin: 5px;
+      background-color: rgba(0, 0, 0, 0.404);
+      padding: 1vh;
+      justify-content: center;
     }
 
-    /*FOOOOOOOOOOOOOOOOOOOOOTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEERRRRRRRRR*/
-
-    #footer {
-        position: relative;
-        display: flex;
-        background-color: #291601;
-        height: auto;
-        align-items: center;
-        justify-content: center;
-    }
-    #findUs {
-        list-style-type: none;  
-        text-decoration: none;
-        display: inline-flex;
-        column-gap: 20px;
-        margin-left: -40px;
-    }
-    #findUs img {
-        height: 30px;
-        width: 30px;
-        &:hover{
-            mix-blend-mode: multiply;
-            transition:.3s;
-        }
-    }
-    
+    /* Registration Form */
     .container {
       position: relative;
-      text-align: center;
       background-color: #052501;
-      padding:10px; 
+      padding: 20px;
       border-radius: 8px;
       box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
       width: 400px;
       font-size: 40px;
     }
-    .message{
-        padding: 40px;
-        border-radius: 8px;
-        font-size: 15px;
+
+    .message {
+      padding: 0px;
+      border-radius: 8px;
+      font-size: 15px;
     }
 
     h2 {
@@ -117,36 +97,37 @@
     form {
       display: flex;
       flex-direction: column;
-      padding-top: 40px;
+      padding-top: 10px;
       font-size: 25px;
-      color:#fff;
+      color: #fff;
     }
 
     label {
       margin-bottom: 8px;
+      margin-left: 5px;
       font-weight: bold;
     }
 
     input {
       padding: 10px;
-      margin-bottom: 16px;
+      margin-bottom: 5px;
       border: 1px solid #ccc;
       border-radius: 4px;
     }
 
     input[type="submit"] {
-      background-color: rgb(250, 201, 151);
+      background-color: rgb(112, 60, 0);
       color: #fff;
       cursor: pointer;
       font-size: 25px;
-    }
-
-    input[type="submit"]:hover {
-      background-color: rgb(10, 148, 10);
+      &:hover {
+        background-color: rgb(10, 148, 10);
+        border: 1px solid rgb(10, 148, 10);
+      }
     }
 
     .form-group {
-      margin-bottom: 20px;
+      margin-bottom: 10px;
     }
 
     .form-group::after {
@@ -158,108 +139,160 @@
     .form-group label,
     .form-group input {
       float: left;
-      width: 48%;
+      width:49%;
       box-sizing: border-box;
     }
 
     @media screen and (max-width: 600px) {
+
       .form-group label,
       .form-group input {
         width: 100%;
       }
     }
+
     #signIn {
-            margin-top: 10px;
-            color: rgb(10, 148, 10);
-            font-size: 18px;
-            &:hover {
-                color: grey;
-                text-decoration: underline;
-            }
-        }
+      margin-top: 10px;
+      color: rgb(10, 148, 10);
+      font-size: 18px;
+
+      &:hover {
+        color: grey;
+        text-decoration: underline;
+      }
+    }
+
+    /* Drop down Login */
+    #dropdown-container {
+        display: none;
+        position: absolute;
+        background-color: grey;
+        border-radius: 20%;
+        width:90px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
+    #dropdown-container a {
+        color: black;
+        padding: 5px;
+        font-size: 20px;
+        border-right: none;
+    }
+    #dropdown-container a:hover {
+        background-color: #ddd;
+    }
+    #dropdown:hover #dropdown-container {
+        display: block;
+    }
+
+    /*FOOOOOOOOOOOOOOOOOOOOOTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEERRRRRRRRR*/
+    #footer {
+      position: relative;
+      display: flex;
+      background-color: #291601;
+      height: auto;
+      align-items: center;
+      justify-content: center;
+    }
+
+    #findUs {
+      list-style-type: none;
+      text-decoration: none;
+      display: inline-flex;
+      column-gap: 20px;
+      margin-left: -40px;
+    }
+
+    #findUs img {
+      height: 30px;
+      width: 30px;
+
+      &:hover {
+        mix-blend-mode: multiply;
+        transition: .3s;
+      }
+    }
+
     
-</style>
+  </style>
 </head>
+
 <body>
-    <div id="mainBody">
+  <div id="mainBody">
+    <header id="header">
+      <nav id="nav"> <img style="padding-left: 2%; width: 70px; border-radius: 80px; padding-top: 1%; padding-bottom: 1%" src="Pics/Logo-Pics/logo.png" />
+        <h1 style=" padding: 20px 15px 5px 5px;"><span style="color: rgb(10, 148, 10);">Agriculture</span> & <span style=" color:rgb(250, 201, 151);">Food Production</span></h1>
+        <div id="menu"> 
+          <a href="homeAFP.html"><span>Home</span></a> 
+          <a href="aboutAFP.html"><span>About</span></a> 
+          <a href="products.html"><span>Products</span></a> 
+          <a href="servicesAFP.html"><span>Services<span></a> 
+          <div id="dropdown">
+          <a style="text-decoration: underline rgb(197, 115, 47); color: rgb(197, 115, 47);" href="#" style="border: none;"><span>Log-in</span></a> 
+            <div id="dropdown-container">
+                <a class="dropdown-content" href="http://localhost/AFP/customerLogin.php">Customer</a>
+                <a class="dropdown-content" href="http://localhost/AFP/loginAFP.php">Employee</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
 
-        <header id="header">
-            <nav id="nav">
-                <img style="padding-left: 2%; width: 70px; border-radius: 80px; padding-top: 1%; padding-bottom: 1%" src="Pics/Logo-Pics/logo.png"/>
-                <h1 style=" padding: 20px 15px 5px 5px;"><span style="color: rgb(10, 148, 10);">Agriculture</span> & <span style=" color:rgb(250, 201, 151);">Food Production</span></h1>
-                <div id="menu">
-                    <a class="rightBorder" href="homeAFP.html"><span>Home</span></a>
-                    <a class="rightBorder" href="aboutAFP.html"><span>About</span></a>
-                    <a class="rightBorder" href="products.html"><span>Products</span></a>
-                    <a class="rightBorder" href="servicesAFP.html"><span>Services<span></a>
-                    <a id="login" style="text-decoration: underline rgb(197, 115, 47); color: rgb(197, 115, 47);" href="loginAFP.html" style="border: none;"><span>Log-in</span></a>
-                </div>
-            </nav>
-        </header>
+    <main id="main">
+      <div class="container"> <center style="margin-bottom:10px;"><span style="color: rgb(10, 148, 10);">Agriculture</span> <span style="color: white;">&</span> <span style=" color:rgb(250, 201, 151);">Food Production</span></center>
+        <div class="message"> <center style="margin-bottom:1  0px;"><span style="color: white;">Sign Up and Harvest from our Agriculture & Food Production Website</span></center>  
+          <form action="#" method="post">
+            <div class="form-group"> <label for="email">Email:</label> <input type="email" id="email" name="uEmail" placeholder="Email" value="<?php echo isset($_POST['uEmail']) ? htmlspecialchars($_POST['uEmail']) : ''; ?>" required> </div>
+            <div class="form-group"> <label for="name">Full Name:</label> <input type="text" id="name" name="uName" placeholder="Name" value="<?php echo isset($_POST['uName']) ? htmlspecialchars($_POST['uName']) : ''; ?>" required> </div>
+            <div class="form-group"> <label for="username">Username:</label> <input type="text" id="username" name="uUsername" placeholder="Username" value="<?php echo isset($_POST['uUsername']) ? htmlspecialchars($_POST['uUsername']) : ''; ?>" required> </div>
+            <div class="form-group"> <label for="password">Password:</label> <input type="password" id="password" name="uPassword" placeholder="Password" required> </div> 
+            <input type="submit" value="Register" name="sign-up">
+            <hr width="100%">
+            <div style="display: flex; justify-content: center; text-align: center;"><a href="http://localhost/AFP/loginAFP.php" id="signIn">Sign-in</a></div>
+          </form>
 
-        <main id="main">
-            <div class="container">
-              <span style="color: rgb(10, 148, 10);">Agriculture</span>
-              <span style="color: white;">&</span>
-              <span style=" color:rgb(250, 201, 151);">Food Production</span>
-                <div class="message">
-                  <span style="color: white;">Sign Up and Harvest from our Agriculture & Food Production Website</span>
-                    
-                  <form action="#" method="post">
-                    <div class="form-group">
-                      <label for="email">Email:</label>
-                      <input type="email" id="email" name="uEmail" required>
-                    </div>
+          <?php $DBHost = "localhost"; //hostname 
+        $DBUser = "root"; //username 
+        $DBPass = ""; //password 
+        $DBName = "afpdb"; //Name 
+        $conn = mysqli_connect($DBHost, $DBUser, $DBPass, $DBName); 
+        if(!$conn) {die("Connection failed:" . mysqli_connect_error());} 
 
-                    <div class="form-group">
-                      <label for="name">Full Name:</label>
-                      <input type="text" id="name" name="fName" required>
-                    </div>
+        if(isset($_POST['sign-up'])) {
+          $checkQuery = "SELECT * FROM customer WHERE customer_Username = '$_POST[uUsername]'";
+          $checkResult = mysqli_query($conn, $checkQuery);
 
-                    <div class="form-group">
-                      <label for="username">Username:</label>
-                      <input type="text" id="username" name="uName" required>
-                    </div>
+            if (mysqli_num_rows($checkResult) > 0){
+              echo "<div style='color: #c51919'><center>Username is already taken. Please choose a different username.</center></div>";
+            } elseif (strlen($_POST['uPassword']) < 8 ) {
+              $errorStylePassword = ";";
+              echo "<div style='color: #c51919'><center>Password must be at least 8 characters.</center></div>";
+            } else {
+              $sql = "INSERT into customer (`customer_Email`,`customer_Name`,`customer_Username`,`customer_Password`) 
+              values('$_POST[uEmail]','$_POST[uName]','$_POST[uUsername]','$_POST[uPassword]')"; 
+              
+              $result = mysqli_query($conn,$sql);
+              if ($result) {
+              echo "<div style='color: GREEN'><center>Registered Successfully</center></div>";
+              } else { echo "<div style='color: #c51919'><center>Registration failed. Please try again.</center></div>";}  
+            }
+          }
+?>
+        </div>
+      </div>
+    </main>
 
-                    <div class="form-group">
-                      <label for="password">Password:</label>
-                      <input type="password" id="password" name="uPassword" required>
-                    </div>
-
-                      <input type="submit" value="Register" name="sign-up">
-                      <hr width="100%">
-                      <div style="display: flex; justify-content: center; text-align: center;"><a href="http://localhost/AFP/loginAFP.php" id="signIn">Sign-in</a></div>
-                      
-                  </form>
-                </div>
-
-                    <?php
-                      $DBHost = "localhost"; //hostname
-                      $DBUser = "root"; //username
-                      $DBPass = ""; //password
-                      $DBName = "afpdb"; //Name
-                      $conn = mysqli_connect($DBHost, $DBUser, $DBPass, $DBName);
-                        if(!$conn) {die("Connection failed:" . mysqli_connect_error());}
-
-                            if(isset($_POST['sign-up']) !='') {
-			                        $sql = "INSERT into employee (`employee_Email`,`employee_Name`,`employee_Username`,`employee_PW`)
-			                        values('$_POST[uEmail]','$_POST[fName]','$_POST[uName]','$_POST[uPassword]')";
-			                        $result = mysqli_query($conn,$sql);
-			                        echo "<br>Registered Successfully";
-		                        }
-                    ?>
-        </main>
-
-        <footer id="footer">
-          <h2 style="font-family: Lucida Console; color:rgb(255, 255, 255); position: absolute; left: 10px;">GROUP 10 <span style="color: rgb(10, 148, 10);"></h2>
-          <h3 style="font-family: Lucida Console; position: absolute; right: 30px; bottom: 5px;">Contact Us: (63+)9123456789</h3>
-          <ul id="findUs">
-              <li><a href="#"><img src="Pics/Icon-pics/facebook.png"/></a></li>
-              <li><a href="#"><img src="Pics/Icon-pics/instagram.png"/></a></li>
-              <li><a href="#"><img src="Pics/Icon-pics/pinterest.png"/></a></li>
-          </ul>
-      </footer>
-
-    </div>
+    <footer id="footer">
+      <h2 style="font-family: Lucida Console; color:rgb(255, 255, 255); position: absolute; left: 10px;">GROUP 10 <span style="color: rgb(10, 148, 10);"></h2>
+      <h3 style="font-family: Lucida Console; position: absolute; right: 30px; bottom: 5px;">Contact Us: (63+)9123456789
+      </h3>
+      <ul id="findUs">
+        <li><a href="#"><img src="Pics/Icon-pics/facebook.png" /></a></li>
+        <li><a href="#"><img src="Pics/Icon-pics/instagram.png" /></a></li>
+        <li><a href="#"><img src="Pics/Icon-pics/pinterest.png" /></a></li>
+      </ul>
+    </footer>
+  </div>
 </body>
+
 </html>
